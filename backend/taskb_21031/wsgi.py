6c0b6dcadb4c5416c50040ef6f63f1c27831cabb
Crowdbotics <team@crowdbotics.com>
2020-10-03 19:28:02 +0000

    Initial commit.

---
+++ backend/taskb_21031/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for taskb_21031 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskb_21031.settings')
+
+application = get_wsgi_application()
